a1f64959bf21d4e8eb4b76a71ed9d0f15e11f527
gparlakov <gparlakov@gmail.com>
2013-04-18 03:08:17 -0700

    JS part 2. HW 1 DOM Manipulation: Task 2 - Beta - moving divs stable working with minor fixes - starting position of divs is outside of screen, and other stuff for fancy

--- JavaScriptHWpart2/1.DomManipulation/2.MoveDivsInCircle/scripts/movableDiv.js
+++ JavaScriptHWpart2/1.DomManipulation/2.MoveDivsInCircle/scripts/movableDiv.js
@@ -1,29 +1,30 @@
 
-// posTop and posLeft is the center of new object
+// posTop and posLeft is the center of rotation of new object
 // speed from 0.03 to 0.1 
 // moves in circles
-function createMovableDiv(posTop,posLeft,speed){
-	if (!speed) {
-		var speed = 0.02;
+// positions new element ouside of screen
+function createMovableDiv(posTop,posLeft,spd){
+	if (!spd) {
+		var spd = 0.02;
 	}	
 	return { 	
 		centerTop : posTop,
 		centerLeft : posLeft,
-		positionTop : posTop,
-		positionLeft : posLeft,
-		speed:speed,
+		positionTop : -1000,
+		positionLeft : -1000, 
+		speed:spd,
 		degree:0,			
 		element:document.createElement("div")
 	}
 }
 
 function updateElement(movableDiv) {
-	movableDiv.element.style.top = movableDiv.positionTop;
-	movableDiv.element.style.left = movableDiv.positionLeft;
+	movableDiv.element.style.top = movableDiv.positionTop + "px";
+	movableDiv.element.style.left = movableDiv.positionLeft + "px";
 }
 
 function updateDegree(movableDiv) {
-	if(movableDiv.degree > Math.PI * 2){
+	if(movableDiv.degree >= Math.PI * 2 - movableDiv.speed){
 		movableDiv.degree = 0;
 	}
 	else{			
@@ -31,9 +32,9 @@ function updateDegree(movableDiv) {
 	}
 }
 
-function moveCircular(movableDiv) {
+function moveCircular(movableDiv) {	
 	updateDegree(movableDiv);				
 	movableDiv.positionTop = movableDiv.centerTop + Math.cos(movableDiv.degree) * 100;
-	movableDiv.positionLeft = movableDiv.centerLeft + Math.sin(movableDiv.degree) * 100;
+	movableDiv.positionLeft = movableDiv.centerLeft + Math.sin(movableDiv.degree) * 100;	
 	updateElement(movableDiv);
 }
