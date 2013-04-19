4e3512768af9f43200e019dca4d28583d91300c7
gparlakov <gparlakov@gmail.com>
2013-04-19 08:17:03 -0700

    JS part 2. HW 1 DOM Manipulation: Task 3 - Step 1 - Getting DOMTree Complete. //TODO add logic for searching the tree :)

--- JavaScriptHWpart2/1.DomManipulation/3.QuerySelectorShim/scripts/querySelector.js
+++ JavaScriptHWpart2/1.DomManipulation/3.QuerySelectorShim/scripts/querySelector.js
@@ -1,14 +1,45 @@
 function addQueries(){
-	document.querySelectorAll = querySelectorAllShim(selectors);
-	//document.prototype.querySelectorAll = querySelectorAllShim;
+	
+	document.querySelectorAll = querySelectorAllShim;
+	document.querySelector = function(selectors) {
+		var findAll = querySelectorAllShim(selectors);
+		return findAll[0];
+	}
 }
 
 function querySelectorAllShim(selectors){
-	var resultElement = null;
+	var resultElement = new Array();;
+	var i;
 	
-	if(args.indexOf(',') > 0){
-		var multipleSelectorLines = selectors.split(',');
+	if(selectors.indexOf(',') > 0){
+		var selectorsSplit = selectorsString.split(',');		
+		for(i = 0; i < selectorsSplit.length; i++){
+			resultElement.push(querySelectorAllShim(selectorsSplit[i]));
+		}
+	}
+	else{
+		if(selectors.indexOf(' ') > 0){			 
+		}
 	}
 	
-	return "working" + selectors;
+	return resultElement;
+}
+
+function getNodesTree(htmlObject){
+	var nodesTree = new Object();	
+	var nextNode = htmlObject.firstChild;	
+	while(nextNode != null){		
+		nodesTree[nextNode.nodeName] = getNodesTree(nextNode);
+		nextNode = nextNode.nextSibling;
+	}
+	
+	return nodesTree;
+}
+
+function clearTextNodes(array){
+	var cleanedArray = new Array();
+	for(var i = 0;i < array.length;i++){
+		//if(typeof array[i] !== 'TextNode'){}
+		console.log(typeof array[i]);
+	}
 }
