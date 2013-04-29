1b50f6b13b1c949ee2a11498d631c02f30cb98b8
gparlakov <gparlakov@gmail.com>
2013-04-29 08:14:35 -0700

    Advanced Functions Task 1: DomModule to expose only methods

---
+++ JavaScriptHWpart2/2.AdvancedFunctions/1.GenerateRandomDivs/scripts/domModule.js
@@ -0,0 +1,25 @@
+﻿var domModule = function () {
+    var addChild = function (element, parentSelector) {
+        var parent = document.querySelector(parentSelector);
+        parent.appendChild(element);
+    }
+
+    var delChild = function (elementSelector, parentSelector) {
+        var parent = document.querySelector(parentSelector);
+        var elem = parent.querySelector(elementSelector);
+        elem.outerHTML = "";
+    }
+
+    var attachEv = function (elementSelector, eventType, eventHandler) {
+        var elem = document.querySelectorAll(elementSelector);
+        for (var i = 0; i < elem.length; i++) {
+            elem[i][eventType] = eventHandler;            
+        }
+    }
+
+    return {
+        addChild: addChild,
+        deleteChild: delChild,
+        attachEvent:attachEv
+    }
+}();
