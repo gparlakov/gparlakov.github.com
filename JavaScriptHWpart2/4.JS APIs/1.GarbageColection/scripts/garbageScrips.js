6632e1aa466f8167679803b5fba8bb014cd23277
gparlakov <gparlakov@gmail.com>
2013-05-08 07:41:36 -0700

    JS part 2. HW 4. JS API's TrashCan opens and closes in time

--- JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
+++ JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
@@ -1,36 +1,39 @@
 ﻿/* File Created: май 7, 2013 */
-(function () {
-    var trashCan = document.querySelector('.trash-can');
-    trashCan.ondragenter = openCan;
-    var garbage = document.querySelectorAll('img');
-    for (var i = 0; i < garbage.length; i++) {
-        garbage[i].ondrop = recycleGarbage;
-    }
-} ());
 
-var openCan = function (ev) {
+
+(function () {
+	var openCan = function (ev) {
     if (!ev) {
         ev = window.event;
     }
     toggleCan(ev.currentTarget);
     ev.stopPropagation();
-};
+	};
 
-var closeCan = function (can) {
-    toggleCan(can);    
-}
+	var closeCan = function (ev) {
+		toggleCan(ev.currentTarget);    
+	}
 
-var toggleCan = function (elem) {
-    elem.classList.toggle("closed");
-    elem.classList.toggle("opened");
-};
+	var toggleCan = function (elem) {
+		elem.classList.toggle("closed");
+		elem.classList.toggle("opened");
+	};
 
-var recycleGarbage = function (ev) {
-    if (!ev) ev = window.event;
+	var recycleGarbage = function (ev) {
+		if (!ev) {
+			ev = window.event;
+		}
+		ev.currentTarget.style.display="none";
+		ev.stopPropagation();
+	}
 
     var trashCan = document.querySelector('.trash-can');
-    toggleCan(trashCan);
-    ev.currentTarget.style.display="none";
-    ev.stopPropagation();
-
-}
+    trashCan.ondragenter = openCan;
+	trashCan.ondragover = function() {return false;};
+	trashCan.ondrop = closeCan;
+    var garbage = document.querySelectorAll('img');
+    for (var i = 0; i < garbage.length; i++) {
+		garbage[i].ondrag = function(ev){ev.stopPropagation(); return false;};
+        garbage[i].ondrop = recycleGarbage;
+    }
+} ());
