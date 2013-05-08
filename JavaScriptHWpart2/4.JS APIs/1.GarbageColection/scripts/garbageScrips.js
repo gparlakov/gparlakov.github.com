d8eadfbefa0cada570f1512de4977aa9fed0d2fe
gparlakov <gparlakov@gmail.com>
2013-05-08 14:20:35 -0700

    JS2 - JS APIs - Garbage //TODO finish score keeping

--- JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
+++ JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
@@ -1,23 +1,44 @@
 ﻿/* File Created: май 7, 2013 */
 var gameGarbageCollection = function () {
-    var timeStart = new Date();
-    var createRandomGarbage = function (number) {
-        var newGarbage = document.createElement('img');
-        newGarbage.src = "images/garbage.png";
-        newGarbage.className = "garbage";
-        newGarbage.id = "garbage" + number;
-        newGarbage.style.position = "absolute";
-        newGarbage.style.left = parseInt(300 + Math.random() * 600) + "px";
-        newGarbage.style.top = parseInt(Math.random() * 900) + "px";
-        var garbageContainer = document.getElementsByTagName('body');
-        garbageContainer[0].appendChild(newGarbage);
-    }
+    var timeStart;
+    var numberGarbages;
+    var gameFieldElement = document.getElementsByTagName('body')[0];
 
-    var numberGarbages = parseInt(Math.random() * 10);
+    //Difficulty of game = 10 - 50 pieces of trash to cleanUp 
+    var initialize = function (difficulty, gameFieldElement) {
+        if (!difficulty) {
+            difficulty = 1;
+        }
 
-    for (var i = 0; i < numberGarbages; i++) {
-        createRandomGarbage(i);
-    }
+        if (!gameFieldElement) {
+            gameFieldElement = document.getElementsByTagName('body')[0];
+        }
+
+        numberGarbages = parseInt(difficulty * 10);
+
+        var trashCan = document.createElement('div');
+        trashCan.classList.add("trash-can");
+        trashCan.classList.add("closed");
+        gameFieldElement.appendChild(trashCan);
+
+        var createRandomGarbage = function (number) {
+            var newGarbage = document.createElement('img');
+            newGarbage.src = "images/garbage.png";
+            newGarbage.className = "garbage";
+            newGarbage.id = "garbage" + number;
+            newGarbage.style.left = parseInt(300 + Math.random() * 600) + "px";
+            newGarbage.style.top = parseInt(Math.random() * 700) + "px";
+
+            gameFieldElement.appendChild(newGarbage);
+        };
+
+        for (var i = 0; i < numberGarbages; i++) {
+            createRandomGarbage(i);
+        }
+
+        //starts gamer timer
+        timeStart = new Date();
+    };
 
     var toggleCan = function (ev) {
         if (!ev) {
@@ -28,7 +49,6 @@ var gameGarbageCollection = function () {
         elem.classList.toggle("closed");
         elem.classList.toggle("opened");
         stopEventPropagation(ev);
-
     };
 
     var stopEventPropagation = function (ev) {
@@ -50,24 +70,97 @@ var gameGarbageCollection = function () {
         event.dataTransfer.setData("garbageId", garbageId);
     };
 
-    var trashCan = document.querySelector('.trash-can');
-    trashCan.ondragenter = toggleCan;
-    trashCan.ondragover = stopEventPropagation;
-    trashCan.ondrop = function (ev) {
-        var garbageId = ev.dataTransfer.getData("garbageId");
-        var garbage = document.getElementById(garbageId);
-        garbage.outerHTML = "";
-        toggleCan(ev);
-        numberGarbages--;
-        if (numberGarbages <= 0) {
-            var time = new Date() - timeStart;
-            alert("Game Won! In " + time/1000 + " seconds" );
+    var startGame = function () {
+        var trashCan = document.querySelector('.trash-can');
+        trashCan.ondragenter = toggleCan;
+        trashCan.ondragover = stopEventPropagation; //to allow drop
+        trashCan.ondrop = function (ev) {
+            var garbageId = ev.dataTransfer.getData("garbageId");
+            var garbage = document.getElementById(garbageId);
+            garbage.outerHTML = "";
+            toggleCan(ev);
+            numberGarbages--;
+            if (numberGarbages <= 0) {
+                var time = new Date() - timeStart;
+                alert("Game Won! In " + time / 1000 + " seconds");
+                var playerName = prompt("Nickname for score board: ");
+                scoreKeeping().addPlayerScore(playerName, time / 1000);
+            }
+            return false;
+        };
+
+        var garbage = document.querySelectorAll('img');
+        for (i = 0; i < garbage.length; i++) {
+            garbage[i].ondragstart = drag;
         }
-        return false;
     };
 
-    var garbage = document.querySelectorAll('img');
-    for (i = 0; i < garbage.length; i++) {
-        garbage[i].ondragstart = drag;
+    return {
+        initialize: initialize,
+        startGame: startGame
     }
 };
+
+var scoreKeeping = function () {
+    function copyObjectProperties (obj) {
+        var result = {};
+        for (var i in obj) {
+            result[i] = obj[i];
+        }
+
+        return result;
+    };
+
+    function addPlayerScore (playerName, time) {
+        if (!localStorage.garbageGame) {
+            localStorage.garbageGame = {};
+        };
+
+        localStorage.garbageGame[playerName] = parseFloat(time);
+    }
+
+    function displayScoreBoard () {
+        if (!localStorage.garbageGame) {
+            alert("No Scores");
+            return;
+        }
+
+        var scoreBoardCopy = copyObjectProperties(localStorage.garbageGame);
+        var length = scoreBoardCopy.length;
+        var scores = [];
+
+        for (var i = 0; i < length; i++) {
+            scores.push(pullBestScore(scoreBoardCopy));
+        }
+
+
+    };
+
+    function pullBestScore (scoreBoard) {
+        var bestScore = null;
+        var bestKey = null;
+        for (var i in scoreBoard) {
+            if (bestScore === null) {
+                bestScore = parseFloat(scoreBoard[i]);
+                bestKey = i;
+            }
+            else {
+                if (parseFloat(scoreBoard[i]) < bestScore) {
+                    bestScore = parseFloat(scoreBoard[i]);
+                    bestKey = i;
+                }
+            }
+        }
+
+        var result = i + "   :   " + Math.round(bestScore, 3);
+        scoreBoard[bestKey] = null;
+        return result;
+    }
+
+    return {
+        copyObject: copyObjectProperties,
+        pullBestScore: pullBestScore,
+        displayScoreBoard: displayScoreBoard,
+        addPlayerScore: addPlayerScore
+    };
+}
