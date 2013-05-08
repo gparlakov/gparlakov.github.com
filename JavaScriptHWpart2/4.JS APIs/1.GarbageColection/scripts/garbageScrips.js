116372bcc8397232439077400973761b02839852
gparlakov <gparlakov@gmail.com>
2013-05-08 12:34:04 -0700

    JS2 - JS APIs - Garbage almost done //TODO is adding of score keeping

--- JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
+++ JavaScriptHWpart2/4.JS APIs/1.GarbageColection/scripts/garbageScrips.js
@@ -1,39 +1,73 @@
 ﻿/* File Created: май 7, 2013 */
-
-
-(function () {
-	var openCan = function (ev) {
-    if (!ev) {
-        ev = window.event;
+var gameGarbageCollection = function () {
+    var timeStart = new Date();
+    var createRandomGarbage = function (number) {
+        var newGarbage = document.createElement('img');
+        newGarbage.src = "images/garbage.png";
+        newGarbage.className = "garbage";
+        newGarbage.id = "garbage" + number;
+        newGarbage.style.position = "absolute";
+        newGarbage.style.left = parseInt(300 + Math.random() * 600) + "px";
+        newGarbage.style.top = parseInt(Math.random() * 900) + "px";
+        var garbageContainer = document.getElementsByTagName('body');
+        garbageContainer[0].appendChild(newGarbage);
     }
-    toggleCan(ev.currentTarget);
-    ev.stopPropagation();
-	};
 
-	var closeCan = function (ev) {
-		toggleCan(ev.currentTarget);    
-	}
+    var numberGarbages = parseInt(Math.random() * 10);
 
-	var toggleCan = function (elem) {
-		elem.classList.toggle("closed");
-		elem.classList.toggle("opened");
-	};
+    for (var i = 0; i < numberGarbages; i++) {
+        createRandomGarbage(i);
+    }
 
-	var recycleGarbage = function (ev) {
-		if (!ev) {
-			ev = window.event;
-		}
-		ev.currentTarget.style.display="none";
-		ev.stopPropagation();
-	}
+    var toggleCan = function (ev) {
+        if (!ev) {
+            ev = window.event;
+        }
+
+        var elem = ev.currentTarget;
+        elem.classList.toggle("closed");
+        elem.classList.toggle("opened");
+        stopEventPropagation(ev);
+
+    };
+
+    var stopEventPropagation = function (ev) {
+        if (ev.stopPropagation) {
+            ev.stopPropagation();
+        }
+        if (ev.prevendDefault) {
+            ev.preventDefault();
+        }
+        return false;
+    };
+
+    var drag = function (event) {
+        if (!event) {
+            event = window.event;
+        }
+        var garbageId = event.target.id;
+
+        event.dataTransfer.setData("garbageId", garbageId);
+    };
 
     var trashCan = document.querySelector('.trash-can');
-    trashCan.ondragenter = openCan;
-	trashCan.ondragover = function() {return false;};
-	trashCan.ondrop = closeCan;
+    trashCan.ondragenter = toggleCan;
+    trashCan.ondragover = stopEventPropagation;
+    trashCan.ondrop = function (ev) {
+        var garbageId = ev.dataTransfer.getData("garbageId");
+        var garbage = document.getElementById(garbageId);
+        garbage.outerHTML = "";
+        toggleCan(ev);
+        numberGarbages--;
+        if (numberGarbages <= 0) {
+            var time = new Date() - timeStart;
+            alert("Game Won! In " + time/1000 + " seconds" );
+        }
+        return false;
+    };
+
     var garbage = document.querySelectorAll('img');
-    for (var i = 0; i < garbage.length; i++) {
-		garbage[i].ondrag = function(ev){ev.stopPropagation(); return false;};
-        garbage[i].ondrop = recycleGarbage;
+    for (i = 0; i < garbage.length; i++) {
+        garbage[i].ondragstart = drag;
     }
-} ());
+};
