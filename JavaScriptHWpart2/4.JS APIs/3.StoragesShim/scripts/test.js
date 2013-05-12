78068c9bd597b0cdac8a689a9273f35dd077eb3c
gparlakov <gparlakov@gmail.com>
2013-05-12 01:27:56 -0700

    JS2 - JS APIs - Last Commit is : Finished moving balls demo, This commit is added files to StrageShim demo;

---
+++ JavaScriptHWpart2/4.JS APIs/3.StoragesShim/scripts/test.js
@@ -0,0 +1,12 @@
+﻿function startTest() {
+    consoleP.writeLine('before init');
+    consoleP.writeLine("from local storage : {0}", LocalStorage.getItem('testLocalStorage') || 'undefined');
+    consoleP.writeLine("from session storage : {0}", SessionStorage.getItem('testSessinStorage') || 'undefined');
+
+    LocalStorage.setItem('testLocalStorage', 'fromLocalStorage');
+    SessionStorage.setItem('testSessinStorage', 'fromSessionStorage');
+
+    consoleP.writeLine('After init');
+    consoleP.writeLine("from local storage : {0}", LocalStorage.getItem('testLocalStorage'));
+    consoleP.writeLine("from session storage : {0}", SessionStorage.getItem('testSessinStorage'));
+}
