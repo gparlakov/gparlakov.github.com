200028b7ed6cc442e3b1e80bd503a17a96176c02
gparlakov <gparlakov@gmail.com>
2013-06-11 13:51:04 -0700

    JQuery Overview : Background Changer task added

---
+++ JavaScriptHWpart3/jQueryOverview/jQueryOverview/4.ChangeBackgroundColor/scripts/backgroundChanger.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    var changeColor = function () {
+        $('body').css('background-color', $('.background-changer').val());
+    };
+
+    //$(document).append($("<input type='color' class='background-changer'\/>"));
+    $('.background-changer').on('change', changeColor).on('mouseenter', changeColor)/*.on('mouseleave', function () {
+        $('body').css('background-color',"white");
+    });*/
+});
