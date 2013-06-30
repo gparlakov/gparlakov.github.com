f59a543a467d0491c05197c7d9caede61e0ec50b
gparlakov <gparlakov@gmail.com>
2013-06-30 12:46:45 -0700

    Google+Youtube and FacebookAPI added

---
+++ JavaScriptHWpart3/ThirdPartyAPI/FacebookAPI/scripts/fbApp.js
@@ -0,0 +1,90 @@
+﻿/// <reference path="jquery-2.0.2.js" />
+
+var root;
+$(document).ready(function () {
+    root = $("#fb-root");
+});
+
+window.fbAsyncInit = function () {
+    // init the FB JS SDK
+    FB.init({
+        appId: '534634659932519',                         // App ID from the app dashboard
+        channelUrl: 'http://localhost:3317/channel.html', // Channel file for x-domain comms
+        status: true,                                     // Check Facebook Login status
+        xfbml: true                                       // Look for social plugins on the page
+    });
+   
+    //on successful login loads the buttons and their functions
+    FB.login(function () {
+        //using this for clone base
+        var button = $('<button></button>');
+        
+        var friends = button.clone().html('Show Friends').click(showFriends);
+        var bDay = button.clone().html('Show Birthday').click(showBday);
+        var sendMessage = button.clone().html('Post Message').click(postMessage);
+        var logoutButton = button.clone().html('Log me out').click(function () {
+            FB.logout();
+        });
+
+        root.append(logoutButton).append(friends).append(bDay).append(sendMessage);
+
+    });
+
+    root.on('click', 'img', function () {
+       $(this).toggleClass('bigger-picture');        
+    });
+};
+  
+function showFriends() {
+    var friends = $('<div><p>Friends</p></div>');
+    var img = $('<img alt="User avatar"/>');
+
+    FB.api('/me/friends', function (response) {
+        for (var friend in response.data) {
+            var nextFriendImg = img.clone();
+            nextFriendImg.attr('src', 'https://graph.facebook.com/' + response.data[friend].id + '/picture?type=square');
+            nextFriendImg.addClass('small-picture');
+            friends.append(nextFriendImg);
+        }
+    });
+
+    root.append(friends);
+}
+
+function showBday() {
+    FB.api('/me?fields=birthday', function (response) {
+        root.append($("<div><p>Birthday:</p><p>" + response.birthday + "</p></div>"));
+    })
+}
+
+function postMessage() {
+    FB.ui(
+        {
+            method: 'feed',
+            name: 'The Facebook SDK for Javascript',
+            caption: 'Bringing Facebook to the desktop and mobile web',
+            description: (
+                'A small JavaScript library that allows you to harness ' +
+                'the power of Facebook, bringing the user\'s identity, ' +
+                'social graph and distribution power to your site.'
+            ),
+            link: 'https://developers.facebook.com/docs/reference/javascript/',
+            picture: 'http://www.fbrell.com/public/f8.jpg'
+        },
+        function (response) {
+            if (response && response.post_id) {
+                alert('Post was published.');
+            } else {
+                alert('Post was not published.');
+            }
+        }
+    );
+}
+// Load the SDK asynchronously
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/bg_BG/all/debug.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
---
+++ JavaScriptHWpart3/ThirdPartyAPI/GooglePlusTube/scripts/googlePlusTube.js
@@ -0,0 +1,171 @@
+﻿var player;
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: 'M7lc1UVf-VE',
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
+var helper = (function () {
+    var BASE_API_PATH = 'plus/v1/';
+
+    return {
+        /**
+         * Hides the sign in button and starts the post-authorization operations.
+         *
+         * @param {Object} authResult An Object which contains the access token and
+         *   other authentication information.
+         */
+        onSignInCallback: function (authResult) {
+            gapi.client.load('plus', 'v1', function () {
+                
+                if (authResult['access_token']) {
+                    $('#authOps').show('slow');
+                    $('#gConnect').hide();
+                    helper.profile();
+                    helper.loadYoutubePlayer();                    
+                }
+                else if (authResult['error']) {
+                    // There was an error, which means the user is not signed in.
+                    // As an example, you can handle by writing to the console:
+                    console.log('There was an error: ' + authResult['error']);                    
+                    $('#authOps').hide('slow');
+                    $('#gConnect').show();
+                }
+                //console.log('authResult', authResult);
+            });
+        },
+
+        /**
+         * Calls the OAuth2 endpoint to disconnect the app for the user.
+         */
+        disconnect: function () {
+            // Revoke the access token.
+            $.ajax({
+                type: 'GET',
+                url: 'https://accounts.google.com/o/oauth2/revoke?token=' +
+                    gapi.auth.getToken().access_token,
+                async: false,
+                contentType: 'application/json',
+                dataType: 'jsonp',
+                success: function (result) {
+                    console.log('revoke response: ' + result);
+                    $('#authOps').hide();
+                    $('#profile').empty();
+                    $('#visiblePeople').empty();
+                    $('#authResult').empty();
+                    $('#gConnect').show();
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        },
+
+        /**
+         * Gets and renders the list of people visible to this app.
+         */
+        people: function () {
+            var request = gapi.client.plus.people.list({
+                'userId': 'me',
+                'collection': 'visible'
+            });
+            request.execute(function (people) {
+                $('#visiblePeople').empty();
+                $('#visiblePeople').append('Number of people visible to this app: ' +
+                    people.totalItems + '<br/>');
+                for (var personIndex in people.items) {
+                    person = people.items[personIndex];
+                    $('#visiblePeople').append('<img src="' + person.image.url + '">');
+                }
+            });
+        },
+
+        /**
+         * Gets and renders the currently signed in user's profile data.
+         */
+        profile: function () {
+            var request = gapi.client.plus.people.get({ 'userId': 'me' });
+            request.execute(function (profile) {
+                $('#profile').empty();
+                if (profile.error) {
+                    $('#profile').append(profile.error);
+                    return;
+                }
+                $('#profile').append(
+                    $('<p><img src=\"' + profile.image.url + '\"></p>'));
+                //$('#profile').append(
+                //    $('<p>Hello ' + profile.displayName + '!<br />Tagline: ' +
+                //    profile.tagline + '<br />About: ' + profile.aboutMe + '</p>'));
+                //if (profile.cover && profile.coverPhoto) {
+                //    $('#profile').append(
+                //        $('<p><img src=\"' + profile.cover.coverPhoto.url + '\"></p>'));
+                //}
+            });
+        },
+
+        loadYoutubePlayer: function(){
+            var tag = document.createElement('script');
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+                     
+        }
+
+    };
+})();
+
+/**
+ * jQuery initialization
+ */
+$(document).ready(function () {
+    $('#disconnect').click(helper.disconnect);
+    $('#loaderror').hide();
+    if ($('[data-clientid="YOUR_CLIENT_ID"]').length > 0) {
+        alert('This sample requires your OAuth credentials (client ID) ' +
+            'from the Google APIs console:\n' +
+            '    https://code.google.com/apis/console/#:access\n\n' +
+            'Find and replace YOUR_CLIENT_ID with your client ID.'
+        );
+    }
+});
+
+/**
+ * Calls the helper method that handles the authentication flow.
+ *
+ * @param {Object} authResult An Object which contains the access token and
+ *   other authentication information.
+ */
+function onSignInCallback(authResult) {
+    helper.onSignInCallback(authResult);
+}
